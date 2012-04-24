7b98ee82aaef394b70dcc5bc4cc1af7ae811719f
ogeidix <diegogiorgini@gmail.com>
2012-04-24 23:52:48 +0200

    Remove support for legacy ruby (1.8)

--- lib/yard-rspec.rb
+++ lib/yard-rspec.rb
@@ -1,3 +1,2 @@
 YARD::Templates::Engine.register_template_path File.dirname(__FILE__) + '/../templates'
-require File.join(File.dirname(__FILE__), 'yard-rspec', 'handler') if RUBY19
-require File.join(File.dirname(__FILE__), 'yard-rspec', 'legacy')
+require File.join(File.dirname(__FILE__), 'yard-rspec', 'handler')
